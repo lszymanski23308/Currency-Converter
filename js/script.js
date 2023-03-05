90f4ce347a5230bd48225406f83f3d9f3deaedac
lszymanski23308 <lszymanski23308@gmail.com>
2023-03-05 21:39:30 +0100

    Re Factoring JS functions

--- js/script.js
+++ js/script.js
@@ -1,37 +1,34 @@
-let formElement = document.querySelector(".js-form");
-let currencyElement = document.querySelector(".js-currency");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-
-let EUR = 4.69;
-let USD = 4.37;
-let GBP = 5.32;
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-   
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-
-    let result;
-
-    switch (currency) {
-
-        case "EUR":
-            result = amount / EUR;
-            break;
-
-        case "USD":
-            result = amount / USD;
-            break;
-
-        case "GBP":
-            result = amount / GBP;
-            break;
-    }
-    formElement.addEventListener("reset", (event) => {
+{
+    const calculateResult = (amount, currency) => {
+        const EUR = 4.69;
+        const USD = 4.37;
+        const GBP = 5.32;
+        switch (currency) {
+            case "EUR":
+                return amount / EUR;
+            case "USD":
+                return amount / USD;
+            case "GBP":
+                return amount / GBP;
+        }
+    };
+    const onFormSubmit = (event, resultElement) => {
+        event.preventDefault();
+        const currencyElement = document.querySelector(".js-currency");
+        const amountElement = document.querySelector(".js-amount");
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+        result = calculateResult(amount, currency);
+        resultElement.textContent = result.toFixed(2) + "" + currency;
+    };
+    const onFormReset = (event, resultElement) => {
         resultElement.innerText = "";
-      });
-
-    resultElement.textContent = result.toFixed(2) + " " + currency;
-});
+    };
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const resultElement = document.querySelector(".js-result");
+        formElement.addEventListener("submit", (event) => { onFormSubmit(event, resultElement) });
+        formElement.addEventListener("reset", (event) => { onFormReset(event, resultElement) });
+    };
+    init();
+} 
